0c9fc6bca5e379c7751ddbf21fa738f8ccd936f7
peter.dg <mail@peterdg.com>
2022-11-14 15:25:11 -0600

    init starter

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    printWidth: 80,
+    tabWidth: 4,
+    trailingComma: 'all',
+    singleQuote: true,
+    semi: true,
+    importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
